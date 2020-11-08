e5a3821d871df7fec1e5a20e238ae4badcd543cc
Joao Meyer <1994meyer@gmail.com>
2020-11-08 17:58:39 -0300

    Now it is fluent

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='pfluid',
+    name='pfluent',
     version='0.0.1',
-    description='Thin wrapper that makes subprocess more fluid to work with',
-    url='https://github.com/meyer1994/pfluid',
-    download_url='https://github.com/meyer1994/pfluid/archive/v0.0.1.tar.gz',
+    description='Thin wrapper around subprocess to make use of fluent syntax',
+    url='https://github.com/meyer1994/pfluent',
+    download_url='https://github.com/meyer1994/pfluent/archive/v0.0.1.tar.gz',
     author='João Vicente Meyer',
     license='GPL-3.0',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools'
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
-    keywords='subprocess run popen bash fluid',
+    keywords='subprocess run popen bash fluent',
     packages=find_packages(exclude=['tests']),
     install_requires=[],
     python_requires='>=3.6',
